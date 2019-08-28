fbbaabfafe27e828baddf2de392074f4e1f7705d
charliepaull <charliepaull211@gmail.com>
2019-08-28 12:07:49 -0700

    starting components, state in DDContainer.js

---
+++ dropdown-add/src/Components/DDContainer.js
@@ -0,0 +1,53 @@
+import React from "react"; 
+
+
+// this has to be a class component with a constructor because it's holding the state
+class DDContainer extends React.Component {
+    constructor() {
+        super();
+
+        // state will have the Options (selected) & toggle (isOpen)
+        this.state = {
+            // using integers here because selections will be made by IDs (unique keys for each data point)
+            selected: 0,
+            // if toggle is open or not 
+            isOpen: false
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                Hello!
+            </div>
+        )
+    }
+}
+
+// data - list of cities - array of objects
+const cities = [
+    {
+        id: 1,
+        location: "Los Angeles"
+    }, 
+    {
+        id: 2,
+        location: "San Francisco"
+    },
+    {
+        id: 3,
+        location: "Barcelona"
+    }, 
+    {
+        id: 4,
+        location: "Tokyo"
+    }, 
+    {
+        id: 5,
+        location: "Atlanta"
+    }
+
+]
+
+
+export default DDContainer;
